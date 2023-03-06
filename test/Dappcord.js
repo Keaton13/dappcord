efbe72fb40e9cc1e2e4d318c1f7e49888daa72f1
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2023-03-06 15:40:51 -0800

    First test

--- test/Dappcord.js
+++ test/Dappcord.js
@@ -1,9 +1,26 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 describe("Dappcord", function () {
+  
+  describe("Deployment", function () {
+    it("Sets the name & symbol", async () => {
+      // Deploy Contract
+      const Dappcord = await ethers.getContractFactory("Dappcord");
+      dappcord = await Dappcord.deploy("Dappcord", "DC")
 
-})
+      // Fetch name
+      let result = await dappcord.name();
+      // Check name
+      expect(result).to.equal("Dappcord")
+
+      // Fetch symbol
+      result = await dappcord.symbol()
+      // Check symbol
+      expect(result).to.equal("DC")
+    });
+  });
+});
